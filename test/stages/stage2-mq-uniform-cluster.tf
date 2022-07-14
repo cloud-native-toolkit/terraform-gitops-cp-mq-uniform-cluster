a66afddbff8581f6dcbb294cf52a4511ed0dd085
Gowdhaman J <gowdhaman_j@yahoo.com>
2022-07-14 08:16:41 +0530

    Updated for CP4i 2022.2.1 (#8)
    Signed-off-by: gowdhaman-j <gowdhaman_j@yahoo.com>

--- test/stages/stage2-mq-uniform-cluster.tf
+++ test/stages/stage2-mq-uniform-cluster.tf
@@ -7,11 +7,12 @@ module "gitops_module" {
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
-  kubeseal_cert = module.gitops.sealed_secrets_cert
+  #kubeseal_cert = module.gitops.sealed_secrets_cert
+  kubeseal_cert = module.cert.cert
   entitlement_key = module.cp_catalogs.entitlement_key
 
   #Uniform Cluster specifics
-  storageClass="portworx-db2-rwo-sc"
+  storageClass="ibmc-vpc-block-10iops-tier"
 
 
 }
