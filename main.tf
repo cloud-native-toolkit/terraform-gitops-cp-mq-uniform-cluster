cc52d4597f468b633d2778a43150270efa5be36a
Bala Sivasubramanian <bala@us.ibm.com>
2022-09-28 13:48:05 -0500

    update gitops provider (#10)
    support for gitops provider and gitea
    
    Signed-off-by: balasgit <bala@us.ibm.com>
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    Co-authored-by: Sean Sundberg <seansund@us.ibm.com>
    Co-authored-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ locals {
   namespace = var.namespace
 
   name          = "gitops-cp-mq-uniform-cluster"
-  bin_dir       = module.setup_clis.bin_dir
   
   base_name          = "ibm-mq-uniform-cluster"
   instance_name      = "${local.base_name}-instance"
@@ -147,37 +146,29 @@ locals {
 
 
 #MQ Uniform Cluster instance is one of the heavy workload component. Hence It is better to manage this in a separate namespace
-module "mq_uniform_cluster_instance_ns" {
-    source = "github.com/cloud-native-toolkit/terraform-gitops-namespace.git"
-    
-
-  gitops_config = var.gitops_config
-  git_credentials = var.git_credentials
+resource gitops_namespace ns {
   name = local.namespace
-  
-} 
-
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+  server_name = var.server_name
+  branch = local.application_branch
+  config = yamlencode(var.gitops_config)
+  credentials = yamlencode(var.git_credentials)
 }
 
-module pull_secret {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
-
-  depends_on = [
-    module .mq_uniform_cluster_instance_ns
-  ]
-
-  gitops_config = var.gitops_config
-  git_credentials = var.git_credentials
+resource gitops_pull_secret cp_icr_io {
+  name = "ibm-entitlement-key"
+  namespace = local.namespace
   server_name = var.server_name
+  branch = local.application_branch
+  layer = local.layer
+  credentials = yamlencode(var.git_credentials)
+  config = yamlencode(var.gitops_config)
   kubeseal_cert = var.kubeseal_cert
-  #namespace = var.namespace
-  namespace= local.namespace
-  docker_username = "cp"
-  docker_password = var.entitlement_key
-  docker_server   = "cp.icr.io"
+
+
   secret_name     = "ibm-entitlement-key"
+  registry_server = "cp.icr.io"
+  registry_username = "cp"
+  registry_password = var.entitlement_key
 }
 
 resource null_resource create_yaml {
@@ -190,38 +181,17 @@ resource null_resource create_yaml {
   }
 }
 
-resource null_resource setup_gitops {
-  depends_on = [null_resource.create_yaml,module.mq_uniform_cluster_instance_ns]
+resource gitops_module setup_gitops {
+  depends_on = [null_resource.create_yaml]
 
-  triggers = {
-    name = local.instance_name
-    #namespace = var.namespace
-    namespace= local.namespace
-    yaml_dir = local.yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = local.type
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-    bin_dir = local.bin_dir
-  }
-
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
+  name = local.instance_name
+  namespace = local.namespace
+  content_dir = local.yaml_dir
+  server_name = var.server_name
+  layer = local.layer
+  type = local.type
+  branch = local.application_branch
+  config = yamlencode(var.gitops_config)
+  credentials = yamlencode(var.git_credentials)
 }
+
